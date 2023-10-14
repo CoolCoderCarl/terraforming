326d9155a7c96c05cf0b12299e6800c3876d6770
CoolCoderCarl <123qwekir2wl@gmail.com>
2023-10-14 19:00:07 +0300

    Add vpc & networks

--- variables.tf
+++ variables.tf
@@ -1,3 +1,22 @@
+variable "vpc_name" {
+  description = "Value of the Name tag for the VPC name"
+  type        = string
+  default     = "TestProjectVPC"
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  description = "Public Subnet CIDR values"
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  description = "Private Subnet CIDR values"
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
@@ -7,5 +26,5 @@ variable "instance_name" {
 variable "ami_id" {
   description = "Value of the AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-023d1ab98b72c7720"
+  default     = "ami-053b0d53c279acc90"
 }
