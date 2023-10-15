5a71ad5a402a083a33d3b0e56cd54909ca1146f2
CoolCoderCarl <123qwekir2wl@gmail.com>
2023-10-15 20:06:34 +0300

    Format & add dynamodb

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  subnet_id     = "${aws_subnet.public_subnets.id}"
+  subnet_id     = aws_subnet.public_subnets.id
   #subnet_id     = "${var.private_subnet}"
   tags = {
     Name = var.instance_name
--- main.tf
+++ main.tf
@@ -13,3 +13,21 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "dynamodb_table" {
+  source = "terraform-aws-modules/dynamodb-table/aws"
+
+  name     = "herold_database"
+  hash_key = "id"
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "testing"
+  }
+}
